0de5908dfb7cb2895058963c5eeccd7a404e390b
ninz0018 <ninzgetil25@gmail.com>
2024-03-10 04:38:41 -0800

    port

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).on(`click`, function(e){
+    e.preventDefault();
+    $("#tiktok").wrap(`<a href="https://www.tiktok.com/en/" target="_blank" </a>`)
+    $("#utube").wrap(`<a href="https://www.youtube.com/" target="_blank" </a>`)
+})
