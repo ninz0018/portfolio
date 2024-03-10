80cd5fab6e4c5d0993549c176b5573a8ac0432ac
ninz0018 <ninzgetil25@gmail.com>
2024-03-10 23:24:09 +0800

    tut5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(document).on(`click`, function(e){
-    e.preventDefault();
-    $("#tiktok").wrap(`<a href="https://www.tiktok.com/en/" target="_blank" </a>`)
-    $("#utube").wrap(`<a href="https://www.youtube.com/" target="_blank" </a>`)
-})
+$(document).ready(function() { 
+    $("#tiktok").wrap(`<a href="https://www.tiktok.com/en/" target="_blank"></a>`);
+    $("#utube").wrap(`<a href="https://www.youtube.com/" target="_blank"></a>`);
+    $("#me").wrap(`<a href="https://ninzgradecalculator.netlify.app/" target="_blank"></a>`);
+  });
