82b0a591442e6ef07b581f286fdfd2806ff0de29
ninz0018 <ninzgetil25@gmail.com>
2024-03-11 22:48:26 +0800

    portfolio

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 $(document).ready(function() { 
     $("#tiktok").wrap(`<a href="https://www.tiktok.com/en/" target="_blank"></a>`);
-    $("#utube").wrap(`<a href="https://www.youtube.com/" target="_blank"></a>`);
+    $("#utube").wrap(`<a href="https://www.youtube.com/channel/UCpPea51kxBmNYyWEIDW3C1A" target="_blank"></a>`);
     $("#me").wrap(`<a href="https://ninzgradecalculator.netlify.app/" target="_blank"></a>`);
+    $("#but").wrap(`<a href="https://getbootstrap.com/" target="_blank"></a>`);
+    $('body').scrollspy({ target: '#navbar' });
+    $("#prof").wrap(`<a href="https://www.facebook.com/ninz.getz.9" target="_blank"></a>`);
   });
 
-
-  function highlight(element) {
-    let navItems = document.querySelectorAll('.nav-item'); // Get all nav items
+ $(document).on(`click`,`#prof`,function(){
     
-    // Remove 'active' class from all nav items
-    navItems.forEach(function(item) {
-      item.classList.remove('active');
-    });
-    
-    // Add 'active' class to the clicked nav item
-    element.parentNode.classList.add('active');
-  }
-  
+ })
