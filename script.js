f3ac527fd5897613476c592b1d62002db22dba4d
ninz0018 <ninzgetil25@gmail.com>
2024-03-11 18:46:26 -0800

    portfolio

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ $(document).ready(function() {
     $("#utube").wrap(`<a href="https://www.youtube.com/" target="_blank"></a>`);
     $("#me").wrap(`<a href="https://ninzgradecalculator.netlify.app/" target="_blank"></a>`);
   });
+
+
+  function highlight(element) {
+    let navItems = document.querySelectorAll('.nav-item'); // Get all nav items
+    
+    // Remove 'active' class from all nav items
+    navItems.forEach(function(item) {
+      item.classList.remove('active');
+    });
+    
+    // Add 'active' class to the clicked nav item
+    element.parentNode.classList.add('active');
+  }
+  
